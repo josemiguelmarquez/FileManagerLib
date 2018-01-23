3a7b14d596eca331b82479abe8d018e7e6036e15
jmmarquezjimenez <jmmarquezjimenez@gmail.com>
2018-01-23 19:10:38 +0100

    A user should be able to read an XML file

---
+++ FileManagerLib/Instances/XMLReader.cs
@@ -0,0 +1,54 @@
+﻿using FileManagerLib.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace FileManagerLib.Instances
+{
+    /// <summary>
+    /// Instance of the reader to read XML files
+    /// </summary
+    public class XMLReader : IReader
+    {
+        /// <summary>
+        /// Read XML file
+        /// </summary>
+        /// <param name="filePath">path to the xml file</param>
+        /// <returns>XML file content as a string</returns>
+        public string Read(string filePath)
+        {
+            string result = string.Empty;
+
+            // Check correct path
+            if (File.Exists(filePath) && (filePath.EndsWith(".xml")))
+            {
+                using (XmlTextReader reader = new XmlTextReader(filePath))
+                {
+                    while (reader.Read())
+                    {
+                        switch (reader.NodeType)
+                        {
+                            case XmlNodeType.Element: // The node is an element.
+                                result += ("<" + reader.Name);
+                                result += (">");
+                                break;
+                            case XmlNodeType.Text: //Display the text in each element.
+                                result += (reader.Value);
+                                break;
+                            case XmlNodeType.EndElement: //Display the end of the element.
+                                result += ("</" + reader.Name);
+                                result += (">");
+                                break;
+                        }
+                    }
+                }       
+            }
+
+            return result;
+        }
+    }
+}
