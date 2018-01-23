2119fcb1218bee8ec6317dfe355ba3ae187ddac4
jmmarquezjimenez <jmmarquezjimenez@gmail.com>
2018-01-23 18:38:28 +0100

    A user should be able to read a text file

---
+++ FileManagerLib/Instances/TextReader.cs
@@ -0,0 +1,33 @@
+﻿using FileManagerLib.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManagerLib.Instances
+{
+    /// <summary>
+    /// Instance of the reader to read TEXT files
+    /// </summary>
+    public class TextReader : IReader
+    {
+        public string Read(string filePath)
+        {
+            // Check correct path
+            if (File.Exists(filePath) && (filePath.EndsWith(".txt")))
+            {
+                using (FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                {
+                    using (StreamReader streanReader = new StreamReader(fileStream))
+                    {
+                        return streanReader.ReadToEnd();
+                    }
+                }
+            }
+
+            return string.Empty;
+        }
+    }
+}
---
+++ FileManagerLib/Interfaces/IReader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManagerLib.Interfaces
+{
+    /// <summary>
+    /// Interface to read Text files
+    /// </summary>
+    public interface IReader
+    {
+        /// <summary>
+        /// Read a plain TXT file
+        /// </summary>
+        /// <param name="filePath">path to the file</param>
+        /// <returns>the content of the file</returns>
+        string Read(string filePath);
+    }
+}
