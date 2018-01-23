babd7f7d6e66ab60650aa8922e172744fdded5d9
jmmarquezjimenez <jmmarquezjimenez@gmail.com>
2018-01-23 20:28:13 +0100

    a user should be able to read an encrypted XML file

--- FileManagerLib/Instances/XMLReader.cs
+++ FileManagerLib/Instances/XMLReader.cs
@@ -12,17 +12,20 @@ namespace FileManagerLib.Instances
     /// <summary>
     /// Instance of the reader to read XML files
     /// </summary
-    public class XMLReader : IReader
+    public class XMLReader : IReader, ICryptoReader
     {
         private IRoleProvider _roleProvider;
+        private IEncryptor _encryptor;
 
         /// <summary>
         /// Constructor that enables DI
         /// </summary>
         /// <param name="roleProvider">Role provider selected</param>
-        public XMLReader(IRoleProvider roleProvider)
+        /// /// <param name="encryptor">the cryptographer selected</param>
+        public XMLReader(IRoleProvider roleProvider, IEncryptor encryptor)
         {
             this._roleProvider = roleProvider;
+            this._encryptor = encryptor;
         }
 
         public string Read(string filePath)
@@ -56,6 +59,42 @@ namespace FileManagerLib.Instances
                         }
                     }
                 }                
+            }
+
+            return result;
+        }
+
+        public string Read(string filePath, bool encryptedText)
+        {
+            string result = string.Empty;
+
+            // Check correct path
+            if (File.Exists(filePath) && (filePath.EndsWith(".xml")))
+            {
+                // Role base security for the XML reading logic
+                if (_roleProvider.CanReadXMLFile(filePath))
+                {
+                    using (XmlTextReader reader = new XmlTextReader(filePath))
+                    {
+                        while (reader.Read())
+                        {
+                            switch (reader.NodeType)
+                            {
+                                case XmlNodeType.Element: // The node is an element.
+                                    result += (!encryptedText ? ("<" + reader.Name) : _encryptor.Decrypt("<" + reader.Name));
+                                    result += (!encryptedText ? (">") : _encryptor.Decrypt(">")); 
+                                    break;
+                                case XmlNodeType.Text: //Display the text in each element.
+                                    result += (!encryptedText ? (reader.Value) : _encryptor.Decrypt(reader.Value)); 
+                                    break;
+                                case XmlNodeType.EndElement: //Display the end of the element.
+                                    result += (!encryptedText ? ("</" + reader.Name) : _encryptor.Decrypt("</" + reader.Name)); 
+                                    result += (!encryptedText ? (">") : _encryptor.Decrypt(">")); 
+                                    break;
+                            }
+                        }
+                    }
+                }
             }
 
             return result;
