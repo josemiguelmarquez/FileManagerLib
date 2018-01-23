7e0752e7c4149860b26d2b1692e8e29a6ee5c111
jmmarquezjimenez <jmmarquezjimenez@gmail.com>
2018-01-23 20:47:21 +0100

    A user should be able to read JSON files

---
+++ FileManagerLib/Instances/JSONReader.cs
@@ -0,0 +1,41 @@
+﻿using FileManagerLib.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManagerLib.Instances
+{
+    /// <summary>
+    /// Instance of the reader to read JSON files
+    /// </summary>
+    public class JSONReader : IReader
+    {
+        /// <summary>
+        /// For the Read method of the JSON files we will implement the same logic as the 
+        /// TEXT file since the scope does not say anything regarding the json objects itself.
+        /// If we want to parse and use the JSON object in the .NET environment I suggest
+        /// to include the NewtonSoft library
+        /// </summary>
+        /// <param name="filePath">path to the file</param>
+        /// <returns>content of the json file</returns>
+        public string Read(string filePath)
+        {
+            // Check correct path
+            if (File.Exists(filePath) && (filePath.EndsWith(".json")))
+            {
+                using (FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                {
+                    using (StreamReader streanReader = new StreamReader(fileStream))
+                    {
+                        return streanReader.ReadToEnd();
+                    }
+                }   
+            }
+
+            return string.Empty;
+        }
+    }
+}
